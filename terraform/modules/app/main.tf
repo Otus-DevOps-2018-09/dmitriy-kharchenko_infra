9d7152f332c1190e4d6ad014efef7a0ff8c9714f
Dmitriy Kharchenko <d.s.harchenko@mpt.ru>
2018-10-27 19:23:23 +0300

    Add modules and environments for Terraform

---
+++ terraform/modules/app/main.tf
@@ -0,0 +1,53 @@
+data "template_file" "puma_service" {
+  template = "${file("${path.module}/files/puma.service.tpl")}"
+
+  vars {
+    db_url = "${var.db_url}"
+  }
+}
+
+resource "google_compute_instance" "app" {
+  name         = "reddit-app"
+  machine_type = "g1-small"
+  zone         = "${var.zone}"
+  tags         = ["reddit-app"]
+
+  boot_disk {
+    initialize_params {
+      image = "${var.app_disk_image}"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config = {
+      nat_ip = "${google_compute_address.app_ip.address}"
+    }
+  }
+
+  metadata {
+    ssh-keys = "appuser:${file(var.public_key_path)}"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "appuser"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo '${data.template_file.puma_service.rendered}' > /tmp/puma.service",
+    ]
+  }
+
+  provisioner "remote-exec" {
+    script = "${path.module}/files/deploy.sh"
+  }
+}
+
+resource "google_compute_address" "app_ip" {
+  name = "reddit-app-ip"
+}
---
+++ terraform/modules/app/variables.tf
@@ -0,0 +1,21 @@
+variable "app_disk_image" {
+  description = "Disk image for reddit app"
+  default     = "reddit-app-base"
+}
+
+variable "zone" {
+  description = "Zone"
+  default     = "europe-west1-b"
+}
+
+variable "public_key_path" {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "private_key_path" {
+  description = "Path to the public key used for ssh access"
+}
+
+variable "db_url" {
+  description = "Environment variable DATABASE_URL"
+}
