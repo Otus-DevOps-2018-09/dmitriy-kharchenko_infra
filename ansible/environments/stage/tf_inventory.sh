1ef45b14f93d717f7cb781361fecb6c804f719c6
Dmitriy Kharchenko <d.s.harchenko@mpt.ru>
2018-11-08 20:20:07 +0300

    Add environments for Ansible

---
+++ ansible/environments/stage/tf_inventory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd $PROJECT_ROOT/terraform/stage/
+terraform state pull > terraform.tfstate # сохранить состояние из удаленного бакета в файл
+terraform-inventory -list terraform.tfstate # получить из tfstate json для ansible
+rm terraform.tfstate # удалить временный файл
